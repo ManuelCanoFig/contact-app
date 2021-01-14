fca3a2127b09912b262249146fe55965d7379eb8
ManuelCanoFig <jcano@ucol.mx>
2021-01-14 12:17:28 -0600

    Creating the jwt token and adding the jwt secret

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt =  require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 
 //Calling the user model
@@ -12,13 +15,50 @@ router.post('/', [
    check('name','Please add name').not().isEmpty(),
    check('email','Please enter a valid email').isEmail(),
    check('password','Please enter a password with 6 or more characteres').isLength({min: 6})
-],(req, res) =>{
+], async(req, res) =>{
    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    
+    const {name, email, password} = req.body;
 
-   res.send(req.body);
+    try {
+
+      let user = await User.findOne({email: email});
+      if(user){
+         res.status(400).json({msg: 'Email already taken'})
+      }
+
+      user = new User({
+         name: name,
+         email: email,
+         password: password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password,salt);
+
+      await user.save();
+
+      const payload = {
+         user:{
+            id: user.id
+         }
+      }
+      
+      jwt.sign(payload,config.get('jwtSecret'),{
+         expiresIn: 36000
+      }, (error, token) =>{
+         if(error) throw error;
+         res.json({'token':token});
+      });
+       
+    } catch (error) {
+       console.log(error.message);
+       res.status(500).send('Server Error');
+    }
+
 });
 
 module.exports = router;
