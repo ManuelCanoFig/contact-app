f529297af5a4fb65606ef2fcd9af614e20147a64
ManuelCanoFig <jcano@ucol.mx>
2021-02-03 21:24:38 -0600

    catching up to date jjeje

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,34 @@ import React, {Fragment} from 'react';
 import Navbar from './components/layaout/Navbar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar/>
-        </Fragment>
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/about' component={About}/>
-          </Switch>
-        </div>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar/>
+          </Fragment>
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/register' component={Register}/>
+            </Switch>
+          </div>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
